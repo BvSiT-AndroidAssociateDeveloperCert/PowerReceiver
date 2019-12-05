7812a908c4aedc36174e4a14acd50e7f7ce5e1ec
BvSiT <bvstraten1@gmail.com>
2019-12-05 22:39:31 +0100

    Solution

--- app/src/main/java/com/example/powerreceiver/MainActivity.java
+++ app/src/main/java/com/example/powerreceiver/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.powerreceiver;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
      * you can't use static receivers (declared in the mainfest) to receive most Android system broadcasts
      */
     private CustomReceiver mReceiver = new CustomReceiver();
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,27 @@ public class MainActivity extends AppCompatActivity {
         filter.addAction(Intent.ACTION_POWER_CONNECTED);
         // Register the receiver using the activity context.
         this.registerReceiver(mReceiver, filter);
+
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mReceiver
+                        ,new IntentFilter(ACTION_CUSTOM_BROADCAST));
     }
 
     @Override
     protected void onDestroy() {
         //Unregister the receiver
         this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this)
+                .unregisterReceiver(mReceiver);
         super.onDestroy();
+    }
+
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        /** BvS
+         * Note that sendBroadcast(customBroadcastIntent) has to be a member of LocalBroadcastManager,
+         * sendBroadcast(customBroadcastIntent) does not raise an error but does NOT work
+         */
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
     }
 }
